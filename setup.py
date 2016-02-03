9520cc0983604a613213449892e4d25704fef315
Peter Bengtsson <mail@peterbe.com>
2016-02-03 13:03:27 -0500

    warming up

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='medlem',
+    version='0.0.1',
+    description='This is https://github.com/mozilla/medlem',
+    author='Mozilla Corporation',
+    author_email='peterbe@mozilla.com',
+    url='https://github.com/mozilla/medlem'
+)
